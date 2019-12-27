73d25e2085c590a49ebc92c46893191d75f6bd58
Vasyl Matsevko <matsevko.vasil@gmail.com>
2019-12-27 13:28:01 +0200

    change to the task 4

--- js/task4.js
+++ js/task4.js
@@ -1,9 +1,10 @@
 table.onclick = function () {
   let i = 0;
   let j = 0;
-  let two = 0;
-  for(i = 2; i < 10; i++) 
-  for(j = 1; j < 11; j++) 
-    document.write(i+" * "+j+" = "+i * j +"<br />");
-    document.write(+"<br />");
+  for(i = 2; i < 10; i++) {
+    for(j = 1; j < 11; j++) {
+      document.write(`${i} * ${j} = ${i * j}<br />`);
+    } 
+    document.write("<br />");
+  }
 }
